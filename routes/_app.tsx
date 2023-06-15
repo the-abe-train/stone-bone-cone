3ba565f7b5b29efe338162e396c1c5222132132a
Abe Train <abraham.train@gmail.com>
2023-06-15 02:53:07 -0400

    Added Favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -32,6 +32,24 @@ export default function App({ Component }: AppProps) {
           content="A pre-historic battle royale!"
         />
         <meta property="og:site_name" content="Stone, Bone, Cone" />
+        {/* Favicons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
       </Head>
 
       <div
